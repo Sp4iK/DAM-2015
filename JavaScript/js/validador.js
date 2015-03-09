f4e9265b16ef107379fceff7b87c0685c777c722
Sp4iK <ospaiko@gmail.com>
2015-03-10 00:01:43 +0100

    modificado

--- JavaScript/js/validador.js
+++ JavaScript/js/validador.js
@@ -1,4 +1,4 @@
-function valida_texto() {
+function valida_nombre() {
     var valor = document.getElementById("registro_nombre").value;
 
     if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
@@ -9,7 +9,7 @@ function valida_texto() {
 }
 
 function valida_email() {
-    valor = document.getElementById("registro_email").value;
+    var valor = document.getElementById("registro_email").value;
 
 //    if( valor == null || valor.length == 0 || !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor)) ) {
     if( valor == null || valor.length == 0 || !(/([0-9a-zA-Z.-]+)(@)([a-zA-Z]+)([.])([a-zA-Z]{2,3})/.test(valor)) ) {
@@ -18,5 +18,15 @@ function valida_email() {
     }
 }
 
-document.getElementById("registro_nombre").onblur = valida_texto;
+function valida_texto() {
+    var valor = document.getElementById("registro_comentarios").value;
+
+    if( valor == null || valor.length == 0 || valor.length > 50 || /^\s+$/.test(valor) ) {
+        console.log("campo vacio!");
+        return false;
+    }
+}
+
+document.getElementById("registro_nombre").onblur = valida_nombre;
 document.getElementById("registro_email").onblur = valida_email;
+document.getElementById("registro_comentarios").onblur = valida_texto;
