0702575ba365ad6afe4e7315e0f9b2d5f0154989
Sp4iK <ospaiko@gmail.com>
2015-03-10 20:29:23 +0100

    Corregido

--- AJAX/js/ejercicio1.js
+++ AJAX/js/ejercicio1.js
@@ -1,8 +1,10 @@
-var READY_STATE_UNINITIALIZED=0;
-var READY_STATE_LOADING=1;
-var READY_STATE_LOADED=2;
-var READY_STATE_INTERACTIVE=3;
-var READY_STATE_COMPLETE=4;
+var estados = [
+    "READY_STATE_UNINITIALIZED",
+    "READY_STATE_LOADING",
+    "READY_STATE_LOADED",
+    "READY_STATE_INTERACTIVE",
+    "READY_STATE_COMPLETE"
+];
 
 var peticion_http;
 
@@ -30,40 +32,15 @@ function muestraUrl() {
 }
 
 function muestraContenido() {
-    var parrafoEstado = document.createElement("p");
-    var parrafoCodigo = document.createElement("p");
-    var textoEstado = document.createTextNode("");
-    var textoCodigo = document.createTextNode("");
-    parrafoEstado.appendChild(textoEstado);
-    parrafoCodigo.appendChild(textoCodigo);
-    document.getElementById("estados").appendChild(parrafoEstado);
-    document.getElementById("codigo").appendChild(parrafoCodigo);
+    console.log(peticion_http.readyState);
+    document.getElementById("estados").innerHTML += estados[peticion_http.readyState] + "\n";
 
-    switch (peticion_http.readyState) {
-        case READY_STATE_UNINITIALIZED:
-            textoEstado.nodeValue = "READY_STATE_UNINITIALIZED";
-            break;
-        case READY_STATE_LOADING:
-            textoEstado.nodeValue = "READY_STATE_LOADING";
-            break;
-        case READY_STATE_LOADED:
-            textoEstado.nodeValue = "READY_STATE_LOADED";
-            break;
-        case READY_STATE_INTERACTIVE:
-            textoEstado.nodeValue = "READY_STATE_INTERACTIVE";
-            break;
-        case READY_STATE_COMPLETE:
-            textoEstado.nodeValue = "READY_STATE_COMPLETE";
-            textoCodigo.nodeValue = peticion_http.status + ": " + peticion_http.statusText;
-
-            if(peticion_http.status == 200) {
-                var parrafoContenido = document.createElement("p");
-                var textoContenido = document.createTextNode("blabla");
-                document.getElementById("contenidos").appendChild(parrafoContenido.appendChild(textoContenido));
-                document.getElementById("cabeceras").appendChild(document.createElement("p").appendChild(document.createTextNode(""));
-            }
-
-            break;
+    if (peticion_http.readyState == 4) {
+        if (peticion_http.status == 200) {
+            document.getElementById("contenidos").innerHTML = peticion_http.responseText;
+            document.getElementById("cabeceras").innerHTML = peticion_http.getAllResponseHeaders;
+            document.getElementById("codigo").innerHTML = peticion_http.status + ": " + peticion_http.statusText;
+        }
     }
 }
 
