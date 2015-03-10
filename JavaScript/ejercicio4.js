2ad934046d3c534be611dfe295532968b138690a
Sp4iK <ospaiko@gmail.com>
2015-03-10 20:28:59 +0100

    Corregido

--- JavaScript/ejercicio4.js
+++ JavaScript/ejercicio4.js
@@ -3,10 +3,13 @@ var email = "e@mail.com";
 var nombre_apellido = "Markel Flores";
 var etiqueta = "<p>Lorem ipsum dolor sit amet, <script>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <script>quis nostrud</script> exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <script>cillum</script> dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</script></p>";
 
-var regexp1 = /\d{2}\/\d{2}\/\d{2,4}/;
-var regexp2 = /([0-9a-zA-Z.-]+)(@)([a-zA-Z]+)([.])([a-zA-Z]{2,3})/;
+//var regexp1 = /\d{2}\/\d{2}\/\d{2,4}/;
+var regexp1 = /(0[1-9]|[1-2][0-9]|30|31)\/(0[1-9]|1[0-2])\/(\d{4})/;
+//var regexp2 = /([0-9a-zA-Z.-]+)(@)([a-zA-Z]+)([.])([a-zA-Z]{2,3})/;
+var regexp2 = /^(\w+)((\.|-|_)(\w+))*@(\w+)(\.\w{2,})+$/;
 var regexp3 = /([a-zA-Z]+)\s([a-zA-Z]+)/;
-var regexp4 = /<script\w?>.*?<\/script>/g;
+//var regexp4 = /<script\w?>.*?<\/script>/g;
+var regexp4 = /<script\w*?>.*?(<script\w*?>.*?<\/script>\w*?)*<\/script>\w*?/g
 var regexp5 = /[<>&"]/g;
 
 console.log("Texto: "+fecha_nacimiento+"\n"+"Fecha de nacimiento: "+fecha_nacimiento.match(regexp1));
