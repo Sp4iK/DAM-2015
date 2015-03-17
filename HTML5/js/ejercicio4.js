3cd9dfcacb3775a8c527c615a0e2ce4b144fa8e2
unknown <FPE@965PDS3-010.aeg8.edu>
2015-03-17 09:29:42 +0100

    Ejercicio corregido

--- HTML5/js/ejercicio4.js
+++ HTML5/js/ejercicio4.js
@@ -7,7 +7,6 @@ if (Modernizr.input.placeholder) {
 Modernizr.load({
     test: Modernizr.input.placeholder,
     nope: [
-        'css/placeholder_polyfill.min.css',
-        'js/lib/placeholder_polyfill.jquery.min.combo.js'
+        'js/lib/placeholder-polyfill.js'
     ]
 });
