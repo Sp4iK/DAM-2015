1372adcbc79b92082f207b7447f3b6e471fef628
Sp4iK <ospaiko@gmail.com>
2015-04-14 22:34:06 +0200

    Modificaciones

--- Android/TO-DOList/src/com/dam2015/todolist/MainActivity.java
+++ Android/TO-DOList/src/com/dam2015/todolist/MainActivity.java
@@ -1,33 +1,53 @@
 package com.dam2015.todolist;
 
-import com.dam2015.todolist.top_fragment.addItemListener;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements addItemListener {
+public class MainActivity extends Activity {
 
-	public interface ListFragment {
-		public void receiveItem (String item);
-	}
-
-	private ListFragment listFragment;
+	TextView textToAdd;
+	Button btnAdd;
+	ListView list;
+	ArrayAdapter adapter;
+	ArrayList aList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//listFragment = (ListFragment) MainActivity.this;
+		textToAdd = (TextView) findViewById(R.id.editText);
+		btnAdd = (Button) findViewById(R.id.buttonAdd);
+		list = (ListView) findViewById(R.id.listView);
+		aList = new ArrayList();
+		adapter =  new ArrayAdapter(this, android.R.layout.simple_list_item_1, aList);
+		list.setAdapter(adapter);
+		addEventListener();
 	}
 
-	public void addItem (String text) {
-		System.out.println("MainActivity - addItemListener | item: " + text);
+	private void addEventListener() {
+		btnAdd.setOnClickListener(new View.OnClickListener() {
 
-		listFragment.receiveItem(text);
+			@Override
+			public void onClick(View v) {
+				System.out.println("onClick");
+				String text = textToAdd.getText().toString();
+				textToAdd.setText("");
+
+				addItem(text);
+			}
+		});
 	}
 
+	private void addItem (String text) {
+		aList.add(0, text);
+		adapter.notifyDataSetChanged();
+	}
 }
